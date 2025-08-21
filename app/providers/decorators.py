0face1eb07ffac7c9e1c4e92c21f972198d7bc4c
Fernando Espino <mferna.92@gmail.com>
2025-08-21 12:00:06 -0600

    Update to improve decorator

--- app/providers/decorators.py
+++ app/providers/decorators.py
@@ -1,19 +1,29 @@
 from functools import wraps
-from typing import Any, Callable, Type
+from typing import Any, Callable, Type, Optional
 from sqlalchemy.exc import SQLAlchemyError
-from app.providers.exceptions import DatabaseOperationError, BaseException
+from app.providers.exceptions import BaseException, DatabaseOperationError
 from app.core.logger import logger
 
-def handle_db_operation(error_to_raise: Type[BaseException] = DatabaseOperationError):
+def handle_db_operation(error_to_raise: Type[BaseException] = DatabaseOperationError, messsage: Optional[str] = None):
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(self, *args: Any, **kwargs: Any) -> Any:
             try:
                 return func(self, *args, **kwargs)
+            
             except SQLAlchemyError as e:
                 self.session.rollback()
-                message = f"Database operation failed in {func.__name__}"
-                logger.error(f"{message}: {e}", exc_info=True)
-                raise error_to_raise(message)
+                
+                class_name = self.__class__.__name__ if hasattr(self, "__class__") else ""
+                func_location = f"{class_name}.{func.__name__}" if class_name else func.__name__
+
+                if messsage:
+                    logger.error(f"{messsage} in {func_location}: {e}", exc_info=True)
+                    raise error_to_raise(messsage)
+                
+                error_msg = messsage or f"Database operation failed in {func_location}: {e}"
+                logger.error(error_msg, exc_info=True)
+                raise error_to_raise(error_msg)
+            
         return wrapper
     return decorator
