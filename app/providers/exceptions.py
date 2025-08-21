7c35e2869cf14bb59b40859cac6968b207f55164
Fernando Espino <mferna.92@gmail.com>
2025-08-21 13:39:31 -0600

    Update provider exception

--- app/providers/exceptions.py
+++ app/providers/exceptions.py
@@ -1,7 +1,8 @@
 from typing import Any
 
+
 class BaseException(Exception):
-    default_message = "An error occurred"
+    default_message = "An error occurred in provider"
 
     def __init__(self, message: str, *args: Any) -> None:
         self.message = message or self.default_message
@@ -17,11 +18,14 @@ class BaseException(Exception):
 class DatabaseOperationError(BaseException):
     default_message = "Database operation failed"
 
+
 class EntityCreationError(BaseException):
     default_message = "Failed to create entity"
 
+
 class EntityNotFoundError(BaseException):
     default_message = "Entity not found"
+
 
 class EntityFetchError(BaseException):
     default_message = "Failed to fetch entity data"
