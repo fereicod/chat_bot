a51b45fbdcf34940b5b6564c86d916cf87294467
Fernando Espino <mferna.92@gmail.com>
2025-08-21 11:26:36 -0600

    Update decorator to handle exceoptions

--- app/providers/decorators.py
+++ app/providers/decorators.py
@@ -1,18 +1,19 @@
 from functools import wraps
-from typing import Any, Callable
+from typing import Any, Callable, Type
 from sqlalchemy.exc import SQLAlchemyError
-from app.providers.exceptions import DatabaseOperationError
+from app.providers.exceptions import DatabaseOperationError, BaseException
 from app.core.logger import logger
 
-def handle_db_operation():
+def handle_db_operation(error_to_raise: Type[BaseException] = DatabaseOperationError):
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(self, *args: Any, **kwargs: Any) -> Any:
             try:
                 return func(self, *args, **kwargs)
             except SQLAlchemyError as e:
+                self.session.rollback()
                 message = f"Database operation failed in {func.__name__}"
-                logger.error(message, exc_info=True)
-                raise DatabaseOperationError(message)
+                logger.error(f"{message}: {e}", exc_info=True)
+                raise error_to_raise(message)
         return wrapper
     return decorator
